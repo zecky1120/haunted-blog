2c9194b934e5e0ec8d61be64afd73c18c86cebc8
zecky1120 <takahide1120@gmail.com>
2025-10-17 21:18:15 +0900

    fix: コールバックでの分岐はやめて、アクション内で実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,13 +3,14 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
   def show
+    @blog = Blog.find(params[:id])
     raise ActiveRecord::RecordNotFound if @blog.secret? && @blog.user != current_user
   end
 
@@ -46,12 +47,7 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog =
-      if params[:action] == 'show'
-        Blog.find(params[:id])
-      else
-        current_user.blogs.find(params[:id])
-      end
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
